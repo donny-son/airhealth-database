84a141986dabe284d75a72fd9a925f75fa432c5f
Dongook Son <60206749+donny-son@users.noreply.github.com>
2021-11-22 13:09:42 +0900

    Ktdb refactor (#5)

--- KTDB/downloader.py
+++ KTDB/downloader.py
@@ -1,9 +1,9 @@
-import os
+from pathlib import Path
 import asyncio
 import shutil
 
 from yaml import safe_load
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 from tqdm import tqdm
 import aiohttp
 
@@ -13,8 +13,8 @@ class KTDBDownloader:
     LOGIN_URL = BASE_URL + '/loginProcess.do'
     DATA_REQUEST_HISTORY_URL = BASE_URL + '/www/selectMyPbldataReqstWebList.do?key=159'
 
-    CREDENTIAL_PATH = os.path.join(os.path.dirname(__file__), '../credentials/credentials.yml')
-    DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), './downloads')
+    CREDENTIAL_PATH = Path(".") / "credentials" / "credentials.yml"
+    DOWNLOAD_DIR = Path(".") / 'KTDB' / 'downloads'
 
     async def login(self) -> aiohttp.ClientSession:
         # Get credential
@@ -46,12 +46,10 @@ class KTDBDownloader:
         except Exception as e:
             print(f'Failed to login. Reason: {e}')
 
-    async def download(self):
+    async def download(self, batch_size=5):
         # Login
         session = await self.login()
-        if session is None:
-            print("Download can't be started")
-            return
+        assert session is not None, f"session failed"
 
         # Get data page url
         async with session.get(self.DATA_REQUEST_HISTORY_URL) as response:
@@ -66,22 +64,23 @@ class KTDBDownloader:
             table = soup.find('table', class_='board_t1')
             rows = table.find('tbody').find_all('tr')
 
-            print(f'Download start in {self.DOWNLOAD_DIR}')
+            print(f'Start Download in {self.DOWNLOAD_DIR}')
             print('---------------------------')
 
-            batch_size = 5
-            batch_list = [rows[i:i + batch_size] for i in range(0, len(rows), batch_size)]
+            # metadata
+            self._download_metadata(self.DOWNLOAD_DIR / "metadata.html", table)
+
+            # data
+            batch_list = [rows[i:i+batch_size] for i in range(0, len(rows), batch_size)]
             for batch in batch_list:
                 tasks = []
                 for row in batch:
-                    file_name = row.find('td', {'data-cell-header': '자료코드 : '}).string
-
+                    filename = row.find('td', {'data-cell-header': '자료코드 : '}).string
                     download_a_tag = row.find('a', class_="sbtn wgreen")
                     download_url = self.BASE_URL + download_a_tag['href']
+                    file_path = self.DOWNLOAD_DIR / f'{filename}.zip'
 
-                    file_path = f'{self.DOWNLOAD_DIR}/{file_name}.zip'
-                    # Check file already downloaded
-                    if not os.path.isfile(file_path):
+                    if not file_path.is_file():
                         tasks.append(self._download(session, file_path, download_url))
 
                 if tasks:
@@ -92,11 +91,19 @@ class KTDBDownloader:
 
         await session.close()
 
+    def _download_metadata(self, file_path: Path, table: element.Tag):
+        """
+        inputs bs4.element.Tag and writes a human-readable html file
+        """
+        if not file_path.exists():
+            with open(file_path, "w") as f:
+                print(BeautifulSoup(str(table), "html.parser"), file=f)
+
     @staticmethod
-    async def _download(session, file_path, url):
+    async def _download(session, file_path: Path, url):
         async with session.get(url) as response:
             chunk_size = 1024
-            progress_bar = tqdm(desc=file_path, unit='B', unit_scale=True)
+            progress_bar = tqdm(desc=str(file_path), unit='B', unit_scale=True)
             with open(file_path, 'wb') as f:
                 async for chunk in response.content.iter_chunked(chunk_size):
                     f.write(chunk)
@@ -106,15 +113,13 @@ class KTDBDownloader:
     def clean(self):
         no_remove_list = ['README.md']
         print(f'Remove downloads in {self.DOWNLOAD_DIR}')
-        for filename in tqdm(os.listdir(self.DOWNLOAD_DIR)):
-            if filename in no_remove_list:
+        for f in tqdm(self.DOWNLOAD_DIR.iterdir()):
+            if f.name in no_remove_list:
                 continue
-
-            file_path = os.path.join(self.DOWNLOAD_DIR, filename)
             try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
+                if f.is_file() or f.is_symlink():
+                    f.unlink()
+                elif f.is_dir():
                     shutil.rmtree(file_path)
             except Exception as e:
                 print(f'Failed to delete {file_path}. Reason: {e}')
--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from KTDB.downloader import KTDBDownloader
 
 
 async def main(*args, **kwargs):
-    if kwargs["subject"] == "KTDB":
+    if kwargs["Subject"] == "KTDB":
         ktdb_downloader = KTDBDownloader()
-        if kwargs["download"]:
-            await ktdb_downloader.download()
-        if kwargs["clean"]:
+        if kwargs["cmd"] == "download":
+            await ktdb_downloader.download(kwargs["batch_size"])
+        if kwargs["cmd"] == "clean":
             ktdb_downloader.clean()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Data processor for geographic data.")
-    parser.add_argument('subject', metavar='S',help="subject for data processing")
-    parser.add_argument('-d','--download', action="store_true", help="download subject data")
-    parser.add_argument('-c','--clean', action="store_true", help="clean subject data")
+    parser.add_argument('Subject', help="Subject for data processing.")
+    subparser = parser.add_subparsers(dest="cmd", help="Commands to perform on subject.")
+    subparser.required = True
+
+    p_download = subparser.add_parser("download")
+    p_download.add_argument("batch_size", nargs="?", type=int, default=5, help="Batch size of async download. Default is 5.")
+
+    p_clean = subparser.add_parser("clean")
+
     args = parser.parse_args()
     asyncio.run(main(**vars(args)))
