5c0eb354e01ae400448ecf3be7bc7216e335d51e
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-01-13 15:40:13 +0900

    func: geocoding api

--- SGIS/api.py
+++ SGIS/api.py
@@ -77,8 +77,30 @@ class SGISRequest:
         response = requests.get(self.URLS["HADMAREA"], params=params)
         return response.json()
 
+    def geocode_addr(self, address: str, xy_only: bool) -> dict:
+        """geocode string address to utmk coord
+
+        Args:
+            address (str): "대전광역시 서구 청사로 189"
+        """
+        params = {
+                "accessToken": self.TOKEN,
+                "address": address,
+                }
+        response = requests.get(self.URLS["GEOCODING"], params=params)
+        if xy_only:
+            _result = response.json().get('result')
+            if int(_result.get("totalcount")) > 1:
+                raise Warning("Multiple (x,y) returned. Check which (x,y) is correct. Defaults to using the first pair.")
+            _first = _result.get("resultdata")[0]
+            return {'x':_first.get('x'), 'y':_first.get('y')}
+        return response.json()
+
 
 if __name__ == "__main__":
     # print(f"{SGISRequest().get_sido_adm_cd(to_tabular=True)=}")
     # print(f"{SGISRequest().get_adm_cd(cd=11, include_border=1, to_tabular=True)}")
-    print(f"{SGISRequest().get_adm_border()=}")
+    # print(f"{SGISRequest().get_adm_border()=}")
+    res = SGISRequest().geocode_addr('서울특별시 강남구 역삼로 92길 12', xy_only=True)
+    print(f"{type(res)=}")
+    print(f"{res=}")
--- SGIS/urls.py
+++ SGIS/urls.py
@@ -9,6 +9,6 @@ ADM : 행정
 URLS = {
         "AUTH": "https://sgisapi.kostat.go.kr/OpenAPI3/auth/authentication.json",
         "STAGE": "https://sgisapi.kostat.go.kr/OpenAPI3/addr/stage.json",
-        "HADMAREA": "https://sgisapi.kostat.go.kr/OpenAPI3/boundary/hadmarea.geojson"
+        "HADMAREA": "https://sgisapi.kostat.go.kr/OpenAPI3/boundary/hadmarea.geojson",
+        "GEOCODING": "https://sgisapi.kostat.go.kr/OpenAPI3/addr/geocode.json"
         }
-
